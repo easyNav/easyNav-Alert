3511354dc612adbedfe2c1e8b8e6ef0c59022750
nuszhangxin <a0097868@nus.edu.sg>
2014-10-13 12:53:29 +0800

    with the sendalerttime

--- alert.py
+++ alert.py
@@ -2,6 +2,7 @@ import threading
 import json
 import smokesignal
 import Queue
+import datetime
 
 from easyNav_pi_dispatcher import DispatcherClient
 
@@ -26,7 +27,11 @@ class Alert(object):
 
         ## Attach event listeners upon instantiation (to prevent duplicates)
         self.attachEvents()        
-        self.flag=0
+        self.sendflag=1
+		self.state=1    #1-proceed #2-stop #3 step right #4 step left 
+		self.sendAlertInterval = datetime.datetime(seconds=5)
+		self.triggerAlertime=datetime.datetime(seconds=0)
+		
     def start(self):
         """ Start the daemon and run persistently.  Auto-retrieves new map in starting. 
         """
@@ -47,8 +52,13 @@ class Alert(object):
         @smokesignal.on('sonarData')
         def sonardata(args):
 	    threshold=70     
-		#1-proceed #2-stop #3 step right #4 step left 
 		
+		if(currentsoundtime-triggersoundtime>self.sendAlertInterval):
+		   sendflag=1;
+		else 
+		   sendflag=0;
+					
+		#1-proceed #2-stop #3 step right #4 step left 
 		if (self.state ==1): # proceed state
 		  
 		  payload =eval(args.get("payload"))
@@ -56,22 +66,42 @@ class Alert(object):
           rightvalue=payload["2"]
 	      leftvalue=int(leftvalue.rstrip('\u0'))
           rightvalue=int(rightvalue.rstrip('\u0'))	 
-		  if((leftvalue <= threshold) OR(rightvalue <= threshold)):
-			  self.state=2; #stop state
-			  #triggersoundtime=datetime.datetime.now().time() # this is only outputting the time
-              self._dispatcherClient.send(9002, 'say', {'text': 'stop'})
 		  
-	    else if (self.state ==2):	 
+		  if((leftvalue <= threshold) OR(rightvalue <= threshold)):
+			  self.state=2 #stop state
+			  currentsoundtime=datetime.datetime.now().time()
+					
+              if(sendflag==1):
+				self._dispatcherClient.send(9002, 'say', {'text': 'stop'})
+				triggersoundtime=datetime.datetime.now().time() # when it sends
+			    
+		  else
+			   self.state=1 # proceed state
+				
+			
+			
+	    else if (self.state ==2): #stop state	 
 		  if((leftvalue <= threshold) AND(rightvalue >= threshold)):
-			  self.state=3 #step right state
-              self._dispatcherClient.send(9002, 'say', {'text': 'keep right'})
+			  self.state=3 #go to step right state
+			  currentsoundtime=datetime.datetime.now().time()
+			  
+			  if(sendflag==1):
+				self._dispatcherClient.send(9002, 'say', {'text': 'keep right'})
+				triggersoundtime=datetime.datetime.now().time() # when it sends
 		  
 		  else if((leftvalue >= threshold) AND(rightvalue <= threshold)):
-			  self.state=4 #step left state
-              self._dispatcherClient.send(9002, 'say', {'text': 'keep left'})
+			  self.state=4 #go to step left state
+			  currentsoundtime=datetime.datetime.now().time()
+			  
+			  if(sendflag==1):
+				self._dispatcherClient.send(9002, 'say', {'text': 'keep left'})
+				triggersoundtime=datetime.datetime.now().time() # when it sends
 		  
 		  else
-			  self.state =2
+			  self.state =2 #go to proceed state
+			  if(sendflag==1):
+				self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+				triggersoundtime=datetime.datetime.now().time() # when it sends
 		
 		else if (self.state ==3):#keep right 
 			  payload =eval(args.get("payload"))
@@ -81,13 +111,20 @@ class Alert(object):
               rightvalue=int(rightvalue.rstrip('\u0'))	 
 			  
 			  if((leftvalue >= threshold) OR(rightvalue >= threshold)):
-			    self.state=1; #proceed state
-                self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+			    self.state=1; #go to proceed state
+				currentsoundtime=datetime.datetime.now().time()
+				
+				if(sendflag==1):
+				  self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+				  triggersoundtime=datetime.datetime.now().time() # when it sends
 				
 		      else 
-				self.state=2; #proceed stop state
+				self.state=2; #go to stop state
+				if(sendflag==1):
+				self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+				triggersoundtime=datetime.datetime.now().time() # when it sends
 		  			
-		else if (self.state == 4):#keep left
+		else if (self.state == 4):#keep left state
 				
 			  payload =eval(args.get("payload"))
 			  leftvalue= payload["1"]
@@ -96,12 +133,20 @@ class Alert(object):
               rightvalue=int(rightvalue.rstrip('\u0'))	 
 			  
 			  if((leftvalue >= threshold) OR(rightvalue >= threshold)):
-			    self.state=1; #proceed state
-                self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+			    self.state=1; #go to proceed state
+				currentsoundtime=datetime.datetime.now().time()
+				
+                if(sendflag==1):
+				   self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+				   triggersoundtime=datetime.datetime.now().time() # when it sends
 				
 		      else 
-				self.state=2;  #proceed stop state       
-					 
+				self.state=2;  #go to stop state      
+				if(sendflag==1):
+				 self._dispatcherClient.send(9002, 'say', {'text': 'proceed'})
+				 triggersoundtime=datetime.datetime.now().time() # when it sends				
+					
+                 
 def runMain():
     """ Main function called when run as standalone daemon
     """
